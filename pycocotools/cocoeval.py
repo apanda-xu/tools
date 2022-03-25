7de308e984d763ee02400f12e2512bd764311b94
apanda-xu <45070655+apanda-xu@users.noreply.github.com>
2022-03-25 16:32:32 +0800

    Update cocoeval.py

--- pycocotools/cocoeval.py
+++ pycocotools/cocoeval.py
@@ -1,1 +1,17 @@
-import pycocotools
+from pycocotools.coco import COCO
+from pycocotools.cocoeval import COCOeval
+
+if __name__ == "__main__":
+    gt = 'gt.json'
+    dt = 'dt.json'
+    
+    cocoGt = COCO(gt)
+    cocoDt = cocoGt.loadRes(dt)
+    cocoEval = COCOeval(cocoGt, cocoDt, 'bbox')   # 'bbox', 'segm', 'keypoints'
+    
+    # if you want to change the maxDets, user the following code:
+    # cocoEval.params.maxDets = [1,10,10000]
+
+    cocoEval.evaluate()
+    cocoEval.accumulate()
+    cocoEval.summarize()
